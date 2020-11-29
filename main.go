85c12fad622af2e9aa098ec253eb4275873ada2f
marjanmm <marjan.mihanovic@gmail.com>
2020-11-29 18:55:02 +0100

    Simple rest (#1)
    * initial
    
    * test rest v1
    
    * initial
    
    * test rest v1
    
    * test rest v1

--- main.go
+++ main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("nesto")
-	fmt.Println("nova verz")
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
 }
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func homePage(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "welcome to the homepage with method: ", request.Method)
+	fmt.Println("endpoint hit: homepage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
