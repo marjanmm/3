2b3dddfc6d9b431e346b12842c65dd05e5045d55
Marjan mm <marjan.mihanovic@gmail.com>
2020-11-29 18:49:42 +0100

    test rest v1

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequests()
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func homePage(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "welcome to the homepage with method: ", request.Method)
 	fmt.Println("endpoint hit: homepage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
 }
 
 type Article struct {
