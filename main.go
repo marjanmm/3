e63d6f5df79c6abce5f7928eb7770e515095fc75
Marjan mm <marjan.mihanovic@gmail.com>
2020-11-29 15:49:54 +0100

    test rest v1

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("nesto")
-	fmt.Println("nova verz")
+	handleRequests()
 }
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func homePage(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "welcome to the homepage with method: ", request.Method)
+	fmt.Println("endpoint hit: homepage")
+}
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
