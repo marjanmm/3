64f95acb71c7c05fbf7941b07bc6b31dea2aead2
Marjan mm <marjan.mihanovic@gmail.com>
2020-11-29 19:57:09 +0100

    added get all articles and one article

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 	handleRequests()
 }
@@ -21,11 +21,33 @@ func handleRequests() {
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 
 	//http.HandleFunc("/", homePage)
 	//http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returnSingleArticle(writer http.ResponseWriter, request *http.Request) {
+	//defer recoveryFunction(writer)
+	vars := mux.Vars(request)
+	key := vars["id"]
+	found := false
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(writer).Encode(article)
+			found = true
+		}
+	}
+	if found == false {
+		writer.WriteHeader(404)
+	}
+
+}
+
+func recoveryFunction(writer http.ResponseWriter) {
+	fmt.Fprintf(writer, "panic triggered")
 }
 
 func homePage(writer http.ResponseWriter, request *http.Request) {
@@ -39,6 +61,7 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 }
 
 type Article struct {
+	Id      string `json:"Id"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
